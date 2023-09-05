2f770fefd9c37872c42b1d2f97c836d8c3406472
Mark Keeling <mark.keeling@engineering.digital.dwp.gov.uk>
2023-09-05 08:46:16 +0100

    first draft of couples record

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,28 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('*/security-check', function(req, res) {
+    var SecurityConfirm = req.session.data['SecurityCheck'];
+   
+    if(SecurityConfirm == "yes"){
+        res.redirect("personal")
+    }
+    else if(SecurityConfirm == 'no'){
+        res.redirect("security-check-failed")
+    }
+});
+
+router.post('/v4/scenario', function(req, res) {
+    var SecurityConfirm = req.session.data['SecurityCheck'];
+   
+    if(req.session.data['scenario'] == "couple-nsp"){
+        res.redirect("scenario-2")
+    }
+    else{
+        res.redirect("find-someone")
+    }
+});
+
+
+
